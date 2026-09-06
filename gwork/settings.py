60afcf0ad0394eb0c290f79696f55d1cdafa4402
michaekong <micaelndeebai@gmail.com>
2026-09-06 23:41:32 +0100

    config

--- gwork/settings.py
+++ gwork/settings.py
@@ -229,6 +229,7 @@ SPECTACULAR_SETTINGS = {
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",  # Ton frontend React
     "http://127.0.0.1:3000",
+    "https://gworks.netlify.app",
 ]
 
 # Si tu veux autoriser les cookies/credentials
